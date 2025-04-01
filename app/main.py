5f91a4cd457558663bdbdd449e67eb8dfbe145cb
Hanif Barbhuiya <hanifmohammed869@gmail.com>
2025-04-01 14:40:23 +0530

    sending back 200

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ import socket  # noqa: F401
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
+    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 4221))
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    conn, addr = server_socket.accept()
+    result = conn.sendall(response)
+    if result is None:
+        print("Response sent successfully.")
+    else:
+        print("Failed to send response.")
+
+    
 
 
 if __name__ == "__main__":
